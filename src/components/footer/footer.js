172de5fc4e5498177fc439ef9347d63aa53f1944
naman-rohilla-kr <naman.rohilla@kroger.com>
2024-10-28 10:20:44 +0530

    Footer added

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,12 +4,45 @@ import LxtButton from "../buttons/lxtButton";
 import "./footer.scss";
 import SpacedSection from "../spacedSection/spacedSection";
 
+const FooterForm = () => {
+  return (
+    <div className="flex flex-col h-full items-left md:items-center">
+      <div className="pb-8 pt-20 w-1/2 md:w-auto text-xl xl:text-4xl">
+        Stay Informed with News & Events
+      </div>
+      <div className="input-container pb-5 w-full flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+        <input
+          className="h-14 w-full md:w-40 xl:w-80"
+          placeholder="First Name"
+        />
+        <input
+          className="h-14 w-full md:w-40 xl:w-80"
+          placeholder="Email Address"
+        />
+
+        <LxtButton text={"JOIN US"} disableResponsive={true} color={"red"} />
+      </div>
+    </div>
+  );
+};
+
 export default function Footer() {
   return (
     <>
       <SpacedSection
+        backgroundColor="#022359"
+        customStyling="mt-28 md:hidden rounded-2xl translate-y-2"
+        zIndex={10}
+      >
+        <FooterForm></FooterForm>
+        <img
+          className="absolute top-0 right-0 h-72 xl:h-80 -translate-y-24"
+          src="skate2.png"
+        ></img>
+      </SpacedSection>
+      <SpacedSection
         backgroundColor="white"
-        customStyling="mt-10 overflow-hidden"
+        customStyling="mt-0 md:mt-10 overflow-hidden"
       >
         <div className="footer-container border-box pt-0 md:pt-24">
           <div className="form-footer hidden relative rounded-2xl md:flex space-x-4 justify-center w-auto shrink-0">
@@ -17,39 +50,20 @@ export default function Footer() {
               className="absolute left-0 h-64 xl:h-80 xl:w-80 scale-175"
               src="skate1.png"
             ></img>
-            <div className="form-container flex flex-col h-full white items-center">
-              <div className="py-12 text-3xl xl;text-4xl">
-                Stay Informed with News & Events
-              </div>
-              <div className="input-container  flex space-x-4">
-                <input className="h-14 w-40 xl:w-80" placeholder="First Name" />
-                <input
-                  className="h-14 w-40 xl:w-80"
-                  placeholder="Email Address"
-                />
-                <span className="">
-                  <LxtButton
-                    text={"JOIN US"}
-                    disableResponsive={true}
-                    color={"red"}
-                  />
-                </span>
-              </div>
-            </div>
+            <FooterForm></FooterForm>
             <img
               className="absolute right-0 h-64 xl:h-80"
               src="skate2.png"
             ></img>
           </div>
-          <div className="text-black">hi</div>
-          <div className="footer-nav">
+          <div className="footer-nav flex flex-col space-y-6 md:space-y-0 md:space-x-10 md:flex-row w-full justify-center pt-12">
             {menuItems.map((mt) => (
               <Link to={mt.redirect} className="item" exact>
                 {mt.link}
               </Link>
             ))}
           </div>
-          <div className="footer-description text-center pt-8 flex justify-center w-full text-sm">
+          <div className="footer-description text-left md:text-center pt-8 flex justify-center w-full text-sm">
             Our mission is to inspire and empower young skaters, especially
             those who need support the most, to reach their full potential as
             skilled, responsible, and caring athletes.
