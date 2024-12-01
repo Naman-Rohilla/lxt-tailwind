b84bb2e007cc53f9767c0f379df28c1fb4f402a5
naman-rohilla-kr <naman.rohilla@kroger.com>
2024-12-01 17:02:06 +0530

    fixed some bug one

--- src/components/cards/profileCard.js
+++ src/components/cards/profileCard.js
@@ -58,7 +58,7 @@ export default function ProfileCard({ isMobile }) {
 
   return (
     <AnimatePresence>
-      <div className="profile-card h-auto md:h-500 flex flex-col md:flex-row relative">
+      <div className="profile-card h-auto md:h-500 w-full flex flex-col md:flex-row relative">
         <div className="flex items-end w-full  justify-start">
           <motion.img
             initial={{
