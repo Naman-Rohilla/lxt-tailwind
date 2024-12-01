598068e6656f1ac2ea709e3c881902993ac601a9
naman-rohilla-kr <naman.rohilla@kroger.com>
2024-12-01 15:11:23 +0530

    removed the animation delay

--- src/components/animatedDiv/headerDiv.js
+++ src/components/animatedDiv/headerDiv.js
@@ -7,7 +7,7 @@ export default function HeaderDiv({ className, style, children }) {
   return (
     <motion.div
       ref={ref}
-      initial={{ opacity: 0, y: 20 }}
+      initial={{ opacity: 0, y: -20 }}
       animate={isInView ? { opacity: 1, y: 0 } : {}}
       transition={{
         duration: 1,
