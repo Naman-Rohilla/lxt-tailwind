f1c1a17c78084eb51a062f0f948a76dbb2e78c6b
naman-rohilla-kr <naman.rohilla@kroger.com>
2024-12-01 17:37:33 +0530

    fixed some bug one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import LxtButton from "./components/buttons/lxtButton";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isMobileDesktopView, setIsMobileDesktopView] = useState(false);
+
+  useEffect(() => {
+    const checkViewMode = () => {
+      const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+      const isMobileDevice =
+        /android/i.test(userAgent) || /iPhone|iPad|iPod/i.test(userAgent);
+      const isDesktopWidth = window.innerWidth >= 1024;
+      setIsMobileDesktopView(isMobileDevice && isDesktopWidth);
+    };
+
+    checkViewMode();
+    window.addEventListener("resize", checkViewMode);
+    return () => {
+      window.removeEventListener("resize", checkViewMode);
+    };
+  }, []);
   return (
     <>
-      <RouterProvider router={router} />
+      {isMobileDesktopView && (
+        <div className="h-screen w-full flex justify-center items-center">
+          <LxtButton
+            text={"This content is visible on desktop only."}
+            color={"red"}
+          ></LxtButton>
+        </div>
+      )}
+      {!isMobileDesktopView && <RouterProvider router={router} />}
     </>
   );
 }
