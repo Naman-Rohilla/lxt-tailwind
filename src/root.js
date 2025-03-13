86dabbc9f75944b70c2bdbf21a9c66eafd162cad
naman-rohilla-kr <naman.rohilla@kroger.com>
2025-03-13 22:44:40 +0530

    fix the import issue

--- src/root.js
+++ src/root.js
@@ -3,7 +3,7 @@ import Navbar from "./components/navbar/navbar";
 import { useEffect, useState } from "react";
 import Footer from "./components/footer/footer";
 import MobileNavbar from "./components/navbar/mobileNav";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function Root() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
