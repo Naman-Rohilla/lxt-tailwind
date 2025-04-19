851cd890423b6e066624a24e1c4529bc4cfd81c0
Rohilla-Naman <namanrohilla122@gmail.com>
2025-04-19 15:01:07 +0530

    responsive issue

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -30,32 +30,10 @@ export default function Footer() {
   return (
     <>
       <SpacedSection
-        backgroundColor="#022359"
-        customStyling="mt-36 md:mt-28 md:hidden rounded-2xl translate-y-2"
-        zIndex={10}
-      >
-        <FooterForm></FooterForm>
-        <img
-          className="absolute top-0 right-0 h-72 xl:h-80 -translate-y-24"
-          src="https://res.cloudinary.com/djdyzefbx/image/upload/v1738504023/skate2_6_11zon_yibc5o.png"
-        ></img>
-      </SpacedSection>
-      <SpacedSection
         backgroundColor="white"
         customStyling="mt-0 md:mt-10 overflow-hidden"
       >
-        <div className="footer-container border-box pt-0 md:pt-24">
-          <div className="form-footer hidden relative rounded-2xl md:flex space-x-4 justify-center w-auto shrink-0">
-            <img
-              className="absolute left-0 h-64 xl:h-80 xl:w-80 scale-175"
-              src="https://res.cloudinary.com/djdyzefbx/image/upload/v1738504023/skate1_5_11zon_vopqan.png"
-            ></img>
-            <FooterForm></FooterForm>
-            <img
-              className="absolute right-0 h-64 xl:h-80"
-              src="https://res.cloudinary.com/djdyzefbx/image/upload/v1738504023/skate2_6_11zon_yibc5o.png"
-            ></img>
-          </div>
+        <div className="footer-container border-box pt-0">
           <div className="footer-nav flex flex-col space-y-6 md:space-y-0 md:space-x-10 md:flex-row w-full justify-center pt-12">
             {menuItems.map((mt) => (
               <Link to={mt.redirect} className="item" exact>
