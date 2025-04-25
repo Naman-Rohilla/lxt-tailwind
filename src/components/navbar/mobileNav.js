c8c5ff3a51453a3fc091d0cdc71120652a90154e
Rohilla-Naman <namanrohilla122@gmail.com>
2025-04-25 13:19:33 +0530

    fixed the navbar in mobile

--- src/components/navbar/mobileNav.js
+++ src/components/navbar/mobileNav.js
@@ -25,27 +25,24 @@ export default function MobileNavbar() {
     >
       <div
         onClick={() => handleNavigation("/")}
-        className={`flex text-7xs flex-col space-y-1.5 items-center ${
-          location.pathname == "/" ? "text-red-500" : "text-white"
-        }`}
+        className={`flex text-7xs flex-col space-y-1.5 items-center ${location.pathname == "/" ? "text-red-500" : "text-white"
+          }`}
       >
         <img src="lxtlogo.png" className="h-6 w-full" />
         <span>HOME</span>
       </div>
       <div
         onClick={() => handleNavigation("/about")}
-        className={`flex text-7xs space-y-1 flex-col items-center ${
-          location.pathname == "/about" ? "text-red-500" : "text-white"
-        }`}
+        className={`flex text-7xs space-y-1 flex-col items-center ${location.pathname == "/about" ? "text-red-500" : "text-white"
+          }`}
       >
         <img src="about.png" className="h-6 w-7" />
         <span>ABOUT US</span>
       </div>
       <div
         onClick={() => handleNavigation("/rr-lxt-rink")}
-        className={`flex text-7xs space-y-1 flex-col items-center ${
-          location.pathname == "/rr-lxt-rink" ? "text-red-500" : "text-white"
-        }`}
+        className={`flex text-7xs space-y-1 flex-col items-center ${location.pathname == "/rr-lxt-rink" ? "text-red-500" : "text-white"
+          }`}
       >
         <img src="redo.png" className="h-7 w-8" />
         <span>RR LXT LINK</span>
@@ -62,39 +59,35 @@ export default function MobileNavbar() {
           <div className="absolute bottom-[50px] rounded-b-xl rounded-tl-xl right-0 bg-card-color h-40 w-40 flex flex-col pt-2">
             <span
               onClick={() => handleNavigation("/announcement")}
-              className={`p-2 px-4 ${
-                location.pathname == "/announcement"
+              className={`p-2 px-4 ${location.pathname == "/announcement"
                   ? "text-red-500 "
                   : "text-white"
-              }`}
+                }`}
             >
               Announcement
             </span>
             <span
               onClick={() => handleNavigation("/achievement")}
-              className={`p-2 px-4 ${
-                location.pathname == "/achievement"
+              className={`p-2 px-4 ${location.pathname == "/achievement"
                   ? "text-red-500 "
                   : "text-white"
-              }`}
+                }`}
             >
               Achievement
             </span>
             <span
               onClick={() => handleNavigation("/news")}
-              className={`p-2 px-4 ${
-                location.pathname == "/news" ? "text-red-500 " : "text-white"
-              }`}
+              className={`p-2 px-4 ${location.pathname == "/news" ? "text-red-500 " : "text-white"
+                }`}
             >
               Media
             </span>
             <span
               onClick={() => handleNavigation("/contact")}
-              className={`p-2 px-4 ${
-                location.pathname == "/contact" ? "text-red-500 " : "text-white"
-              }`}
+              className={`p-2 px-4 ${location.pathname == "/contact" ? "text-red-500 " : "text-white"
+                }`}
             >
-              Media
+              Contact Us
             </span>
           </div>
         )}
